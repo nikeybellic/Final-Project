b1f6f94c5a5b6d6a52f28b09931174f3ac4511ad
nikeybellic <mannyolowolafe@gmail.com>
2022-05-22 08:53:24 -0400

    Final Project

---
+++ src/data.js
@@ -0,0 +1,78 @@
+const menu = [
+    {
+      id: 1,
+      title: 'Chocolate pancakes',
+      category: 'breakfast',
+      price: 15.99,
+      img: 'src/public/images/choco.jpeg',
+      desc: `These fluffy chocolate pancakes are the perfect way for a chocolate lover to start their day! Thick and full of melty chocolate in each bite `,
+    },
+    {
+      id: 2,
+      title: 'Cheeseburger',
+      category: 'lunch',
+      price: 13.99,
+      img: 'src/public/images/cheese.jpeg',
+      desc: `It's a cheeseburger. What more can be said? `,
+    },
+    {
+      id: 3,
+      title: 'Red Velvet milkshake',
+      category: 'shakes',
+      price: 6.99,
+      img: 'src/public/images/redvelvet.jpeg',
+      desc: `This cupid-approved creamy, dreamy red velvet milkshake is a simply sweet treat for your Valentine`,
+    },
+    {
+      id: 4,
+      title: 'Bacon Egg and Cheese ',
+      category: 'breakfast',
+      price: 4.99,
+      img: 'src/public/images/bec.jpeg',
+      desc: `The NY breakfast staple. Remember to pronounce it as one word when ordering `,
+    },
+    {
+      id: 5,
+      title: 'Egg Salad',
+      category: 'lunch',
+      price: 12.99,
+      img: 'src/public/images/eggsal.jpeg',
+      desc: `
+
+      This is a wonderful-tasting egg salad sandwich that you will definitely devour. It's really good on rye.
+       `,
+    },
+    {
+      id: 6,
+      title: 'Oreo Churros',
+      category: 'shakes',
+      price: 11.99,
+      img: 'src/public/images/oreo.jpeg',
+      desc: `Fried mexican pastry with an Oreo twist`,
+    },
+    {
+      id: 7,
+      title: 'Bagel with Cream Cheese',
+      category: 'breakfast',
+      price: 3.99,
+      img: 'src/public/images/bagel.jpeg',
+      desc: `Simple bagel with cream cheese spread. What more can be said? `,
+    },
+    {
+      id: 8,
+      title: 'Fish tacos',
+      category: 'lunch',
+      price: 12.99,
+      img: 'src/public/images/fish.jpeg',
+      desc: `Like fish or not, you'll absolutely love these tacos!`,
+    },
+    {
+      id: 9,
+      title: 'Caramel Milkshake',
+      category: 'shakes',
+      price: 10.99,
+      img: 'src/public/images/caramel.jpeg',
+      desc: `For all the caramel lovers out. Enjoy a blended milkshake with caramel drizzle on top.`,
+    },
+  ];
+  export default menu;
